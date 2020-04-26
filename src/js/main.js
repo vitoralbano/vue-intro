2e011beb315875fd7be126dc8f0aa66f563cae32
Vitor Albano <vitoralbano@gmail.com>
2020-04-26 12:17:01 -0300

    3-Conditional Rendering

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ var app = new Vue({
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-green.png",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true
     }
 });
